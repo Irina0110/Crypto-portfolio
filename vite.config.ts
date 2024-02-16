fa5dfa406b46456c7c94fbddc8b47aac5960a494
Irina0110 <irinasafonova2001@mail.ru>
2024-02-16 13:29:46 +0300

    fix: deploy url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {fileURLToPath, URL} from 'node:url';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/crypto/",
+  base: "/Crypto-portfolio/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
